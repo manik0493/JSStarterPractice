9c183e03d83c19b33cc89e641daa0c2dba622716
manik0493 <manik.narang@yahoo.com>
2016-11-20 13:59:55 +0530

    added linting support

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,3 @@
 import chalk from 'chalk';
 
-console.log(chalk.green('Starting the app in dev mode...'));
+console.log(chalk.green('Starting the app in dev mode...')); // eslint-disable-line no-console
--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import "./index.css"
 import numeral from 'numeral';
 
 const courseValue =numeral(1000).format('$0,0.00');
-console.log(`I would like to pay ${courseValue} for this course!`);
+console.log(`I would like to pay ${courseValue} for this course!`); 

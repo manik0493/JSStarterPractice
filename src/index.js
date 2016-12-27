deb76d9c0a695c3a54b88eb17296a7654d687376
manik0493 <manik.narang@yahoo.com>
2016-12-27 16:39:07 +0530

    angular material integration

--- src/index.js
+++ src/index.js
@@ -1,33 +1,53 @@
-import {getUsers} from './api/userApi';
+
 import angular from 'angular';
 
 
-angular.module("Myapp",[])
-.controller("MainController",MainController)
 
 
-function MainController($scope)
-{
-  $scope.hello="Hello World!";
+angular.module("Myapp", ['ngMaterial'])
+  .controller("MainController", MainController)
+
+
+function MainController() {
+ 
+}
+angular.module('Myapp').component('helloWorld', {
+  template: `<h2>{{ctrl.msg}}</h2>
+  <button ng-click="ctrl.changeMsg()">Click Me</button>
+  `,
+  controller: helloWorldCtrl,
+  controllerAs: 'ctrl',
+  bindings: {
+    msg: '<',
+    onMsgChanged: '&'
+  }
+});
+
+function helloWorldCtrl() {
+  var vm = this;
+
+  vm.changeMsg = function () {
+    vm.onMsgChanged({ childMsg: 'hello from child' });
+  };
 }
 
-
-
- 
-
-
-getUsers().then(result=>{
-  let usersBody="";
-
-  result.forEach(user=>{
-    usersBody+=`<tr>
-    <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
-    <td>${user.id}</td>
-    <td>${user.firstName}</td>
-    <td>${user.lastName}</td>
-    <td>${user.email}</td>
-    <tr>`
-  });
-
-  global.document.getElementById('users').innerHTML=usersBody;
+angular.module('Myapp').component('myApp', {
+  template: '<hello-world msg="ctrl.msg" on-msg-changed="ctrl.handleMsgChange(childMsg)"></hello-world>',
+  controller: MyAppCtrl,
+  controllerAs: 'ctrl'
 });
+
+MyAppCtrl.$inject = ['$http'];
+function MyAppCtrl($http) {
+  var vm = this;
+
+  vm.handleMsgChange = function (msg) {
+    vm.msg = msg;
+  };
+
+
+  vm.$onInit = function () {
+    vm.msg = "Hello world from my app";
+  };
+
+}
